1bc80a7b16dbc864d82d3839310f6bbd3d5b31af
Admin <admin@MBP-von-Admin.fritz.box>
2023-01-31 19:12:43 +0100

    Initial Commit

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,15 @@
 var dark_mode_input = document.getElementById("toggle-btn-1");
 var wave = document.getElementById("wave")
 var github_icons_elements = document.getElementsByClassName("github-logo");
+var send_icon = document.getElementById("send");
 
 dark_mode_input.addEventListener('change', ()=>{
     document.body.classList.toggle("light-mode")
     
+    if(send_icon.getAttribute("stroke") == "#f8f8f8")
+        send_icon.setAttribute("stroke", "#262626")
+    else
+        send_icon.setAttribute("stroke", "#f8f8f8")
     if(wave.getAttribute("fill") == "#f8f8f8")
         document.getElementById("wave").setAttribute("fill", "#262626")
     else
